04ee70eb4ec74ad035130fa6f72dceec3e5b61a9
jc <jc3464@nau.edu>
2017-05-17 00:32:08 -0700

    latest update

--- codes/Main.py
+++ codes/Main.py
@@ -23,4 +23,26 @@ test_df.info()
 """ start data cleaning for training dataset
     we have 11 raw features and missing value for Age, Cabin, Embarked.
 """
+#%%
 # drop Cabin, too many missed values
+train_df.drop("Cabin",axis=1,inplace=True)
+test_df.drop("Cabin",axis=1,inplace=True)
+# drop danduncy features
+train_df.drop("Ticket",axis=1,inplace=True)
+test_df.drop("Ticket",axis=1,inplace=True)
+
+#train_df.drop("PassengerId",axis=1,inplace=True)
+#test_df.drop("PassengerId",axis=1,inplace=True)
+#%%
+
+
+train_df['Embarked'].value_counts()
+
+train_df["Embarked"] = train_df["Embarked"].fillna("S")
+
+fig = plt.figure()
+ax1 = fig.add_subplot(211)
+ax2 = fig.add_subplot(212)
+
+sns.countplot(x='Survived', hue="Embarked", data=train_df, order=[1,0],ax = ax1)
+sns.countplot(x='Embarked', hue="Survived", data=train_df,ax=ax2)
