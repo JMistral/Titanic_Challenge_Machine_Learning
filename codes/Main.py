2982ba71002eabdd49945eb4dbae014058a0f1ef
JMistral <jchen1105@hotmail.com>
2017-05-18 21:36:49 -0700

    Some figures to visualize data

--- codes/Main.py
+++ codes/Main.py
@@ -50,3 +50,30 @@ sns.countplot(x='Embarked', hue="Survived", data=train_df,ax=ax2)
 # As we see in the second plot, "Embarked" feature is a deterministic factor for "Survived"
 # For example, the passenger embarked through "C" tend to survive rather than those who embarked
 # through "S"
+
+"""
+Now we're interested in the influence of factor, "Pclass" and "Sex"
+"""
+fig = plt.figure()
+sns.countplot(x='Pclass', hue="Survived", data=train_df)
+# seems like the first class passengers have a higher chance to survive
+
+fig = plt.figure()
+sns.countplot(x='Sex', hue="Survived", data=train_df)
+# females are more likely to survive than male
+
+"""
+Dealing with the missed values within feature "Age"
+"""
+age_non_survived = train_df["Age"][train_df["Survived"] == 0]
+age_survived = train_df["Age"][train_df["Survived"] == 1]
+
+#use FacetGrid to plot multiple kdeplots on one plot
+fig = sns.FacetGrid(train_df,hue='Survived')
+#call FacetGrid.map() to use sns.kdeplot() to show age distribution
+fig.map(sns.kdeplot,'Age',shade=True)
+
+#set the x max limit by the oldest passenger
+oldest = train_df['Age'].max()
+fig.set(xlim=(0,oldest))
+fig.add_legend()
